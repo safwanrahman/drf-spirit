9142395cf9d7d6dd25fd9e66b819a353eae6bf9d
Safwan Rahman <safwan.rahman15@gmail.com>
2017-03-24 19:33:00 +0600

    Adding permission for views (#6)

--- drf_spirit/views.py
+++ drf_spirit/views.py
@@ -1,30 +1,36 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .filters import CommentFilter
 from .models import Topic, Category, Comment
+from .permissions import IsOwnerOrReadOnly
 from .serializers import TopicSerializer, CategorySerializer, CommentSerializer
 
 
 class TopicList(generics.ListCreateAPIView):
     serializer_class = TopicSerializer
     queryset = Topic.objects.all()
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
 class TopicDetails(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = TopicSerializer
     queryset = Topic.objects.all()
+    permission_classes = (IsOwnerOrReadOnly,)
 
 
 class CategoryList(generics.ListCreateAPIView):
     serializer_class = CategorySerializer
-    queryset = Category.objects.all()    
+    queryset = Category.objects.all()
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
 class CategoryDetails(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()  
-    lookup_field = 'slug'  
+    lookup_field = 'slug'
+    permission_classes = (IsOwnerOrReadOnly,)
 
 
 class CommentList(generics.ListCreateAPIView):
@@ -32,6 +38,7 @@ class CommentList(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
     filter_backends = (DjangoFilterBackend,)
     filter_class = CommentFilter
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
