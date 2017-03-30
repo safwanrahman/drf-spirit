1b1436ab3fbdc14ae8f7c9e02f4a476ad00906fd
Safwan Rahman <safwan.rahman15@gmail.com>
2017-03-30 06:16:05 +0000

    Adding comment_count increase facility and refactoring signals in topic model, comment_count should be increased when a new comment is made. So when a new comment is made, listen the signal and increase the comment count Refactoring singals and emails so that its possible to receive one signal and do both work (increase comment_count and send email to topic members)

--- drf_spirit/signals.py
+++ drf_spirit/signals.py
@@ -1,28 +1,15 @@
-from django.core.mail import EmailMultiAlternatives
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.template.loader import render_to_string
-from django.utils.translation import ugettext as _
 
-from .emails import send_mass_mail
+from .emails import send_email_to_topic_people
 from .models import Comment
 
 
 @receiver(post_save, sender=Comment)
-def send_email_to_topic_people(sender, instance, created, **kwargs):
-    emails = list(instance.topic.comments.exclude(user=instance.user)
-                          .values_list("user__email", flat=True).distinct().iterator())
-    # append the topic creator email
-    emails.append(instance.topic.user.email)
-    # remove duplicates if the topic creator also comments
-    emails = set(emails)
-    subject = _("A new comment has been made")
-    html_template = "drf_spirit/new_comment.html"
-    html_message = render_to_string(html_template)
-    messages = []
-    for email in emails:
-        msg = EmailMultiAlternatives(subject=subject, to=[email])
-        msg.attach_alternative(html_message, "text/html")
-        messages.append(msg)
+def post_comment_save(sender, instance, created, **kwargs):
+    # Need to send emails to topic people
+    send_email_to_topic_people(instance, created)
 
-    send_mass_mail(messages)
+    # need to update *comment_count of topic if the comment is created
+    if created:
+        instance.topic.increase_comment_count()
